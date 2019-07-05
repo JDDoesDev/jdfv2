5ded7a84f23ae37ca1270e9f7d21370062bdf09f
JD Flynn <jflynn8@gmail.com>
2019-07-05 17:12:02 -0500

    FA5

--- docroot/modules/custom/jdf_custom/src/Plugin/Block/SocialBlock.php
+++ docroot/modules/custom/jdf_custom/src/Plugin/Block/SocialBlock.php
@@ -14,38 +14,55 @@ use Drupal\Core\Block\BlockBase;
  */
 class SocialBlock extends BlockBase {
 
+  public $socialLinkItems;
+
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $this->setSocialLinkItems();
     $build = [];
     $build['social_block']['#markup'] = $this->socialLinks();
 
     return $build;
   }
 
-  public function socialLinks() {
-    $social_links = array_filter([
-      'twitter' => theme_get_setting('bcb_subtheme_social_twitter'),
-      'github' => theme_get_setting('bcb_subtheme_social_github'),
-      'drupal' => theme_get_setting('bcb_subtheme_social_drupal'),
-      'flickr' => theme_get_setting('bcb_subtheme_social_flickr'),
-      'linkedin' => theme_get_setting('bcb_subtheme_social_linkedin'),
-      'paypal' => 'http://paypal.me/JDDoesDev',
-      'envelope' => '/contact',
+  public function setSocialLinkItems() {
+    $this->socialLinkItems = array_filter([
+      'twitter' => [theme_get_setting('bcb_subtheme_social_twitter'), 'fab'],
+      'github' => [theme_get_setting('bcb_subtheme_social_github'), 'fab'],
+      'drupal' => [theme_get_setting('bcb_subtheme_social_drupal'), 'fab'],
+      'linkedin' => [theme_get_setting('bcb_subtheme_social_linkedin'), 'fab'],
+      'paypal' => ['http://paypal.me/JDDoesDev', 'fab'],
+      'patreon' => ['https://www.patreon.com/jddoesthings', 'fab'],
+      'envelope' => ['/contact', 'fas'],
     ]);
+  }
+
+  public function socialLinks() {
 
     $rendered = '<ul class="list-unstyled">';
-    foreach ($social_links as $network => $link) {
+    foreach ($this->socialLinkItems as $network => $link) {
       $rendered .= '<li>';
-      $rendered .= '<a target="_blank" href="' . $link . '">';
+      $rendered .= '<a target="_blank" href="' . $link[0] . '">';
       $rendered .= '<span class="fa-stack fa-lg">';
-      $rendered .= '<i class="fa fa-circle fa-stack-2x"></i>';
-      $rendered .= '<i class="fa fa-' . $network . ' fa-stack-1x fa-inverse"></i>';
-      if ($network == 'envelope') {
-        $network = 'Contact Me';
-      } elseif ($network == 'paypal') {
-        $network = 'Donate via PayPal';
+      $rendered .= '<i class="fas fa-circle fa-stack-2x"></i>';
+      $rendered .= '<i class="' . $link[1] . ' fa-' . $network . ' fa-stack-1x fa-inverse"></i>';
+      switch ($network) {
+        case 'envelope':
+          $network = 'Contact Me';
+          break;
+
+        case 'paypal':
+          $network = 'Donate via PayPal';
+          break;
+
+        case 'patreon':
+          $network = 'Support me on Patreon';
+          break;
+
+        default:
+          break;
       }
       $rendered .= '</span>' . ucfirst($network) . '</a></li>';
     }
---
+++ docroot/modules/custom/jdf_custom/src/Plugin/Block/SocialFooterBlock.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\jdf_custom\Plugin\Block;
+
+use Drupal\jdf_custom\Plugin\Block\SocialBlock;
+
+/**
+ * Provides a 'SocialFooterBlock' block.
+ *
+ * @Block(
+ *  id = "social_footer_block",
+ *  admin_label = @Translation("Social footer block"),
+ * )
+ */
+class SocialFooterBlock extends SocialBlock {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $this->setSocialLinkItems();
+
+    $build = [];
+    $build['social_footer_block']['#markup'] = $this->socialLinks();
+
+    return $build;
+  }
+
+  public function socialLinks() {
+    $rendered = '<ul class="list-inline text-center">';
+    foreach ($this->socialLinkItems as $network => $link) {
+      $rendered .= '<li class="list-inline-item">';
+      $rendered .= '<a target="_blank" href="' . $link[0] . '">';
+      $rendered .= '<i class="' . $link[1] . ' fa-' . $network . '"></i>';
+    }
+    $rendered .= '</ul>';
+
+    return $rendered;
+  }
+
+}
